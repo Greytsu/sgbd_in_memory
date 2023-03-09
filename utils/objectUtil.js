77a813dabf3147b6bc79491ca6ab8818b8c0daef
LucasStrohl <strohllucas93160@gmail.com>
2023-03-09 19:54:53 +0100

    check object structures

---
+++ utils/objectUtil.js
@@ -0,0 +1,24 @@
+exports.InitObject = (props) => {
+    const propsObj = props.reduce((acc, prop) => {
+        acc[prop] = null; 
+        return acc;
+    }, {});
+    return propsObj;
+}
+
+exports.CompareObjectStruct = (obj1, obj2) => {
+    const keys1 = Object.keys(obj1).sort();
+    const keys2 = Object.keys(obj2).sort();
+
+    if (keys1.length !== keys2.length) {
+        return false;
+    }
+
+    for (let i = 0; i < keys1.length; i++){
+        if(keys1[i] !== keys2[i]){
+            return false;
+        }
+    }
+
+    return true;
+}
