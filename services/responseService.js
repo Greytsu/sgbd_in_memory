556ec1542286fd1fcaa7965daf7e494d2a9a28f0
LucasStrohl <strohllucas93160@gmail.com>
2023-03-10 11:11:37 +0100

    add access method header

--- services/responseService.js
+++ services/responseService.js
@@ -1,4 +1,17 @@
 exports.Response = (res, code, message) => {
+    const path = res.req.url.split("?")[0];
+    const pathSplit = path.split("/");
+    
+    if(path === '/' || path === '/status'){
+        res.setHeader("Access-Control-Allow-Methods", "GET")
+    }
+    else if(pathSplit[1] === 'databases' && pathSplit[3] === 'tables'){
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    }
+    else if(pathSplit[1] === 'databases'){
+        res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
+    }
+
     res.writeHead(code, {'Content-Type' : 'application/json'});
     res.end(message);
 }
